52d7de79ae16f7a1b0de3fb94401411d228dcfd7
hlwalZ <ajustytmy@gmail.com>
2024-12-07 23:29:29 +0100

    Tijdelijk

---
+++ Models/pokemon.cs
@@ -0,0 +1,11 @@
+
+namespace Models.pokemon
+{
+  class PokemonList
+  {
+    public string name { get; set; } = "";
+    public string url { get; set; } = "";
+
+  }
+
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+using System.Text.Json;
+using Models.pokemon;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast = Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
+
+
+app.MapGet("/PokemonList", async () =>
+{
+
+
+    HttpClient client = new HttpClient();
+    HttpResponseMessage resu = await client.GetAsync("https://pokeapi.co/api/v2/pokemon?limit=300");
+    resu.EnsureSuccessStatusCode();
+
+
+
+    string responsess = await resu.Content.ReadAsStringAsync();
+    List<PokemonList>? pokes = JsonSerializer.Deserialize<JsonElement>(responsess).GetProperty("results").Deserialize<List<PokemonList>>();
+
+
+
+    return pokes;
+})
+.WithName("PokemonList")
+.WithOpenApi();
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
